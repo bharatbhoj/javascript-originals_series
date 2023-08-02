5a09ba56aeff7676b91cc4781324063c18fbc477
bharatbhoj <140905595+bharatbhoj@users.noreply.github.com>
2023-08-02 11:21:49 +0000

    stringsMethods

---
+++ javaScriptTutorial/06_StringsMethods.js
@@ -0,0 +1,16 @@
+const students=new String('bharat');
+
+
+// console.log(students.splice(students));
+
+
+const para= new String('my name is jack Sparrow i m from califonia.')
+
+//console.log(para.slice(0,23))
+// spice Method :> In this methods extracts a section of a string and returns it as a new string, without modifying the original string.
+
+// console.log(para.replace("jack","Mack")); 
+// replace Methods :> In this methods return a new String with one ,some ,or ALL MATCHES of a pattern replaced by a replacement.
+
+
+console.log(para.indexOf)
