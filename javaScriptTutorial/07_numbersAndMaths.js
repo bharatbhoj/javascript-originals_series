fd81961dfd605089f43b6d34fffd1a40a1b59d81
bharatbhoj <140905595+bharatbhoj@users.noreply.github.com>
2023-08-10 10:54:43 +0000

    numberandmath

---
+++ javaScriptTutorial/07_numbersAndMaths.js
@@ -0,0 +1,35 @@
+const score=new Number(100);
+console.log(score)
+
+const marks=new Number(50)
+// console.log(marks)
+// console.log(marks.toString())
+
+
+const count=new Number(1000000);
+// console.log(count.toLocaleString('en-IN'))
+// console.log(count.toLocaleString())
+
+
+// ++++++++++++++++++++++++Math+++++++++++++++++++++++++++++++++++
+
+
+// console.log(Math)
+// console.log(Math.abs(-9))
+// console.log(Math.round(4.5))
+// console.log(Math.ceil(4.6))
+// console.log(Math.floor(4.9))
+// console.log(Math.min(3,6,9,12,15))
+// console.log(Math.max(3,6,9,12,15))
+
+// console.log(Math.random())
+
+// console.log(Math.random()*20)
+
+// console.log(Math.floor(Math.random()*20)+1)
+  
+
+const min=10;
+const max=20;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
