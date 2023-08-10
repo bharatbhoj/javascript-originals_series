897b06e832cf64b1de8b3b28bb93c983f5ee756e
bharatbhoj <140905595+bharatbhoj@users.noreply.github.com>
2023-08-10 05:54:27 +0000

    stringsMethods

--- javaScriptTutorial/06_StringsMethods.js
+++ javaScriptTutorial/06_StringsMethods.js
@@ -4,13 +4,32 @@ const students=new String('bharat');
 // console.log(students.splice(students));
 
 
-const para= new String('my name is jack Sparrow i m from califonia.')
+const para= new String('my name is jack Sparrow i m from califonia. i love califonia')
 
 //console.log(para.slice(0,23))
-// spice Method :> In this methods extracts a section of a string and returns it as a new string, without modifying the original string.
+// spice Method :> In this methods extracts a section of a string and returns it as a new string, without modifying the original
+//  string.
 
 // console.log(para.replace("jack","Mack")); 
 // replace Methods :> In this methods return a new String with one ,some ,or ALL MATCHES of a pattern replaced by a replacement.
 
+// console.log(para.lastIndexOf("califonia"))
+//lastIndexof():> The lastIndexOf() method returns the index (position) of the last occurrence of a specified value in a string.
 
-console.log(para.indexOf)
+// console.log(para.indexOf)
+// The indexOf() method returns the position of the first occurrence of a value in a string.
+// The indexOf() method returns -1 if the value is not found.The indexOf() method is case sensitive.
+
+// console.log(para.includes('califonia'))
+// include():>The includes() method returns true if a string contains a specified string.
+// Otherwise it returns false.
+// The includes() method is case sensitive.
+
+// console.log(para.replace("califonia","america"))
+// The replace() method searches a string for a value or a regular expression.
+// The replace() method returns a new string with the value(s) replaced.
+// The replace() method does not change the original string.
+
+// console.log(para.substring(0,24))
+// The substring() method extracts characters, between two indices (positions), from a string, and returns the substring.
+
