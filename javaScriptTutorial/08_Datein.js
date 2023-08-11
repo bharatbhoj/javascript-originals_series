61747264b87a595c93dee9d9d1a9314f09e5ed65
bharatbhoj <140905595+bharatbhoj@users.noreply.github.com>
2023-08-11 10:33:13 +0000

    date

---
+++ javaScriptTutorial/08_Datein.js
@@ -0,0 +1,15 @@
+const date=new Date();
+// console.log(date)
+// console.log(date.toString())
+// console.log(date.toLocaleString())
+// console.log(date.toISOString())
+// console.log(date.toDateString())
+
+const myDate=new Date(2023,7,12)
+// console.log(myDate.toDateString())
+
+
+const timeStamp= Date.now()
+// console.log(timeStamp)
+// console.log(Math.floor(timeStamp/1000))
+
