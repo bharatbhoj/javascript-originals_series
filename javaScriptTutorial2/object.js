47adbc56982614191fdc9ee35f38f98137d89a94
bharatbhoj <140905595+bharatbhoj@users.noreply.github.com>
2023-08-15 14:59:07 +0000

    object

--- javaScriptTutorial2/object.js
+++ javaScriptTutorial2/object.js
@@ -4,13 +4,30 @@
 
 Object.create   // constructor methods 
 
-const user ={ name: "bharat",  // actually yeh naa aise dikhta hai  "name"="bharat";
+const sym= Symbol("my_keys1");
+
+const user ={ name: "bharat",  // actually yeh naa aise dikhta hai  "name"="bharat" in real case;
               "full Name":"bharat bhoj",
               age:22,
              location:"delhi",
              email:"bhojbharat07@gmail.com",
              isLoggedIn:false,
-            LastLofinDays: ["monday","friday"]
-            ["symbole"]=bharat        }
+            LastLofinDays: ["monday","friday"],
+            [sym]: "bharat"
+        }
         console.log(user["full Name"])    
-        console.log(["location"])          
+        console.log(["location"])   
+        
+        user.email="bhojbharat07@yahoo.com"
+        console.log(user.email)
+        Object.freeze(user.email)
+
+        user.email="bhojbharat@test.com.com"
+        console.log(user)
+
+
+      user.func= function(){
+        console.log(`hello my world ${this.name}`);
+      }
+
+      console.log(user.func())
