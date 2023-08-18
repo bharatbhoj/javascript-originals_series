4896e8b411e3a18b6e4a860eb5e6f7e5bc5ef82a
bharatbhoj <140905595+bharatbhoj@users.noreply.github.com>
2023-08-18 16:20:58 +0000

    arrow2

--- javaScriptTutorial3/03_arrow.js
+++ javaScriptTutorial3/03_arrow.js
@@ -12,3 +12,39 @@ console.log(this)
 console.log(myObj.welcomeMsg());
 myObj.name="amit"
 console.log(myObj.welcomeMsg());
+
+
+// +++++++++++++++++Arrow++++++++++++++++++++
+
+// const yoyo = function (){
+//     let student="Mehul"
+//     console.log(student);
+// }
+// console.log(yoyo())
+
+
+
+// const yoyo =(num1,num2)=>{
+//     return num1+num2;
+
+
+// }
+// console.log(yoyo(1,99))
+
+// const yoyo =(num1,num2)=>num1*num2;
+
+// console.log(yoyo(1,99))
+
+
+// const yoyo =(num1,num2)=>num1*num2;               //inpresite Return
+// console.log(yoyo(1,99))
+
+
+// const siri=(hello1,hello2)=>( hello1 / hello2)
+
+// console.log(siri(2,10))
+
+const siri=(hello1,hello2)=>({name:"bharatBhoj"})
+
+console.log(siri(2,10))
+
